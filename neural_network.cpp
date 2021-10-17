1747f7e74b1b9aa822b0067733172eb778a3f5d9
Ethan James <ethanjamesauto@vt.edu>
2021-10-17 12:19:33 -0400

    added test main function

--- neural_network.cpp
+++ neural_network.cpp
@@ -6,7 +6,6 @@ Layer::Layer(Tensor& matrix, Tensor& addVector, std::string& activatorType) {
     this->activatorType = activatorType;
 }
 
-
 NeuralNetwork::NeuralNetwork(std::vector<Layer> sequence) {
     this->sequence = sequence;
 }
@@ -17,4 +16,11 @@ using namespace std;
 
 int main() {
     Tensor v1(shared_ptr<float[]>(new float[4]{1, 2, 3, 4}), {4, 1});
+    Tensor v2(shared_ptr<float[]>(new float[3]{5, 6, 7}), {3, 1});
+    Tensor out(0., {4, 3});
+    Tensor::outer_product(v1, v2, out);
+    for (int i = 0; i < out.length; i++) {
+        cout << out.array[i] << " ";
+    }
+    cout << std::endl;
 }
