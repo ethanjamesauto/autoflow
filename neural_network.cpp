8ad073875fab059bf6d49471efa058dba59b5495
Ethan James <ethanjamesauto@vt.edu>
2021-10-17 13:10:24 -0400

    added test MSE gradient function

--- neural_network.cpp
+++ neural_network.cpp
@@ -12,15 +12,27 @@ NeuralNetwork::NeuralNetwork(std::vector<Layer> sequence) {
 
 #include <iostream>
 #include <memory>
+#include <cassert>
 using namespace std;
+
+Tensor gradMse(Tensor& exp, Tensor& actual)  {
+    assert(exp.shape == actual.shape);
+    Tensor grad(0., exp.shape);
+    float scalar = 2. / exp.length;
+    for (int i = 0; i < exp.length; i++) {
+        grad.array[i] = scalar * (exp.array[i] - actual.array[i]);
+    }
+    return grad;
+}
 
 int main() {
     Tensor in({1, 2}, {2, 1});
     Tensor w({1, 2, 3, 4}, {2, 2});
     Tensor out = Tensor::matmult(w, in);
-    Tensor actual({1, 2}, {2, 1});
+    Tensor actual({7, 2}, {2, 1});
+    Tensor grad = gradMse(out, actual);
     for (int i = 0; i < out.length; i++) {
-        cout << out.array[i] << " ";
+        cout << grad.array[i] << " ";
     }
-    cout << std::endl;
+    cout << std::endl << Tensor::mse(out, actual);
 }
