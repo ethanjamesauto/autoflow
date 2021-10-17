6531b0945e9c78fd4bfee5bb33bc2c4128070e2a
Ethan James <ethanjamesauto@vt.edu>
2021-10-17 12:01:16 -0400

    added testing method for neural network class

--- neural_network.cpp
+++ neural_network.cpp
@@ -10,3 +10,11 @@ Layer::Layer(Tensor& matrix, Tensor& addVector, std::string& activatorType) {
 NeuralNetwork::NeuralNetwork(std::vector<Layer> sequence) {
     this->sequence = sequence;
 }
+
+#include <iostream>
+#include <memory>
+using namespace std;
+
+int main() {
+    Tensor v1(shared_ptr<float[]>(new float[4]{1, 2, 3, 4}), {4, 1});
+}
