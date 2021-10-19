3ff46407f1fd5bdf9827679d1b2e3272ce437c2a
Ethan James <ethanjamesauto@vt.edu>
2021-10-19 18:46:33 -0400

    added MSE operation and constructors

--- operators.h
+++ operators.h
@@ -2,6 +2,15 @@
 
 struct MatrixMult : Operation {
     Tensor weights;
+    MatrixMult(Tensor* input, Tensor weights);
+    void execute();
     void gradOp();
     void gradW();
+};
+
+struct MSE : Operation {
+    MSE(Tensor* input)
+        : Operation(input) {}
+    void execute();
+    void gradOp();
 };
