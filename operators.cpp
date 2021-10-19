be074d36a09cc2c23f092b9fbdf8289e69a27578
Ethan James <ethanjamesauto@vt.edu>
2021-10-19 18:43:53 -0400

    started adding several implementations of operators

--- operators.cpp
+++ operators.cpp
@@ -1,9 +1,25 @@
 #include "operators.h"
 
-void MatrixMult::gradOp() {
+Operation::Operation(Tensor* input) {
+    this->input = input;
+}
 
+MatrixMult::MatrixMult(Tensor* input, Tensor weights)
+    : Operation(input) {
+    this->weights = weights;
+}
+
+void MatrixMult::execute() {
+}
+
+void MatrixMult::gradOp() {
 }
 
 void MatrixMult::gradW() {
-    
+}
+
+void MSE::execute() {
+}
+
+void MSE::gradOp() {
 }
