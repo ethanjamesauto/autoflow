a80abb5c0bc801f0ca05db2b1b84e662f8a2097b
Ethan James <ethanjamesauto@vt.edu>
2021-10-20 18:48:44 -0400

    implemented softmax operator

--- operators.cpp
+++ operators.cpp
@@ -1,5 +1,6 @@
 #include "operators.h"
 #include <cassert>
+#include <cmath>
 
 Operation::Operation(Tensor* input) {
     this->input = input;
@@ -23,7 +24,7 @@ MSE::MSE(Tensor* input, Tensor actual)
     : Operation(input) {
     this->actual = actual;
     this->output = Tensor(0., {1});
-    this->gradOperation = Tensor(0., actual.shape);
+    this->gradOperation = Tensor(0., {1, actual.length});
 }
 
 void MSE::execute() {
@@ -40,5 +41,46 @@ void MSE::gradOp() {
 }
 
 Tensor MSE::getGradOp() {
+    return gradOperation;
+}
+
+Softmax::Softmax(Tensor* input)
+    : Operation(input) {
+    this->output = Tensor(0., input->shape);
+    this->gradOperation = Tensor(0., {input->length, input->length});
+}
+
+void Softmax::execute() {
+    double sum;
+    for (int i = 0; i < input->length; i++) {
+        output.array[i] = exp(input->array[i]);
+        sum += output.array[i];
+    }
+    for (int i = 0; i < input->length; i++) {
+        output.array[i] /= sum;
+    }
+}
+
+//TODO: don't make an nxn tensor; for optizimation
+void Softmax::gradOp() {
+    Tensor& z = *input;
+    float x = 0;
+    for (int i = 0; i < z.length; i++) {
+        x += exp(z.array[i]);
+    }
+    for (int k = 0; k < z.length; k++) {
+        for (int l = 0; l < z.length; l++) {
+            float& val = gradOperation.array[k * z.length + l];
+            val = 0;
+            if (k == l) {
+                val += x * exp(z.array[k]);
+            }
+            val -= exp(z.array[k]) * exp(z.array[l]);
+            val /= x * x;
+        }
+    }
+}
+
+Tensor Softmax::getGradOp() {
     return gradOperation;
 }
