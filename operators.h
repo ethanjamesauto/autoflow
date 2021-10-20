0e392b0a7fce0015bd26a756fee0d8787d1f0e94
Ethan James <ethanjamesauto@vt.edu>
2021-10-20 10:26:01 -0400

    updated according to changes in Operator

--- operators.h
+++ operators.h
@@ -1,15 +1,21 @@
 #include "neural_network.h"
 
-struct MatrixMult : Operation {
+class MatrixMult : public Operation {
+    Tensor weights;
+
+   public:
     MatrixMult(Tensor* input, Tensor weights);
     void execute();
     void gradOp();
     void gradW();
 };
 
-struct MSE : Operation {
+class MSE : public Operation {
     Tensor actual;
+
+   public:
     MSE(Tensor* input, Tensor actual);
     void execute();
     void gradOp();
+    Tensor getGradOp();
 };
