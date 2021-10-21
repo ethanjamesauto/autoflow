ffe36bad841f4b6b38c5393ad2df8820dd76b40f
Ethan James <ethanjamesauto@vt.edu>
2021-10-21 11:36:00 -0400

    use static methods instead of mutable methods, added constructor for uninitialized Tensor for optimization

--- tensor.h
+++ tensor.h
@@ -18,19 +18,20 @@ class Tensor {
     Tensor(std::vector<float> array, std::vector<int> shape);
     Tensor(float val, std::vector<int> shape);
     Tensor(const Tensor& t);
+    Tensor(std::vector<int> shape);
 
     void print();
 
-    void reluMutable();
+    static void relu(const Tensor& t, Tensor& out);
     Tensor relu() const;
 
-    void softmaxMutable();
+    static void softmax(const Tensor& t, Tensor& out);
     Tensor softmax() const;
 
-    void addMutable(const Tensor& other);
+    static void add(const Tensor& t, const Tensor& add, Tensor& out);
     Tensor add(const Tensor& other) const;
 
-    void scalarMultMutable(float scalar);
+    static void scalarMult(const Tensor& t, float scalar, Tensor& out);
     Tensor scalarMult(float scalar);
 
     static void matmult(const Tensor& one, const Tensor& two, Tensor& out);
